ab63b98f9fb1a8122de7f09798d6b48683945113
karnag3 <jeff.kelly0110@gmail.com>
2023-05-02 17:23:20 -0500

    update directory intake

--- multi-parser.py
+++ multi-parser.py
@@ -3,9 +3,6 @@ from ttp import ttp
 import os
 import argparse
 
-TEMPLATE_DIR = "arista-ttp-templates/templates"
-DIRECTORY = os.fsencode(TEMPLATE_DIR)
-
 
 def cmdline_args():
     parser = argparse.ArgumentParser()
@@ -16,7 +13,9 @@ def cmdline_args():
         action="store_true",
         help="Print output to screen",
     )
-    parser.add_argument("-t", "--template", type=str, help="Run against a singular template file")
+    parser.add_argument(
+        "-t", "--template", type=str, help="Run against a singular template file"
+    )
     parser.add_argument(
         "-c",
         "--config_file",
@@ -30,6 +29,13 @@ def cmdline_args():
         type=str,
         help="Output file format (options json or yaml)",
         default="yaml",
+    )
+    parser.add_argument(
+        "-d",
+        "--template_directory",
+        type=str,
+        help="Specify different template directory (default is arista-ttp-templates/templates/",
+        default="arista-ttp-templates/templates/",
     )
 
     return parser.parse_args()
@@ -56,23 +62,29 @@ def do_output(output_name, results, do_print):
         print(results)
 
 
-def parse_files(data_to_parse, do_print, template, file_format):
+def parse_files(data_to_parse, do_print, template, file_format, template_dir):
     if template:
         template_file = open(template)
         ttp_template = template_file.read()
         results = run_parser(data_to_parse, ttp_template, file_format)
-        output_name = "outputs/" + template.rsplit("/", 1)[1] + "-outputfile." + file_format
+        output_name = (
+            "outputs/" + template.rsplit("/", 1)[1] + "-outputfile." + file_format
+        )
         do_output(output_name, results, do_print)
     else:
-        for file in os.listdir(DIRECTORY):
+        directory = os.fsencode(template_dir)
+        for file in os.listdir(directory):
             next_file = os.fsdecode(file)
             if next_file.endswith(".ttp"):
                 active_template = file.decode("utf-8")
-                template_file = open("arista-ttp-templates/templates/" + active_template)
+                template_file = open(template_dir + active_template)
                 ttp_template = template_file.read()
                 results = run_parser(data_to_parse, ttp_template, file_format)
                 output_name = (
-                    "outputs/" + active_template.replace(".ttp", "") + "-outputfile." + file_format
+                    "outputs/"
+                    + active_template.replace(".ttp", "")
+                    + "-outputfile."
+                    + file_format
                 )
                 do_output(output_name, results, do_print)
 
@@ -80,7 +92,13 @@ def parse_files(data_to_parse, do_print, template, file_format):
 def main():
     args = cmdline_args()
     data_to_parse = get_config_file(args.config_file)
-    parse_files(data_to_parse, args.do_print, args.template, args.file_format)
+    parse_files(
+        data_to_parse,
+        args.do_print,
+        args.template,
+        args.file_format,
+        args.template_directory,
+    )
 
 
 if __name__ == "__main__":
